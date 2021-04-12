f96a1973ceec6847591d3577d76544148bdfaa93
Radim Capak <radim.capak@gmail.com>
2021-04-12 19:25:42 +0200

    Some Kata training

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {amaroPlan} from "./Katas/pirateGold";
 import {whoTookTheCarKey} from "./Katas/lostKeys";
 import { fortune } from "./Katas/BankersPlan";
+import {beggars} from "./Katas/englishBeggars";
 
 // Pirates coin share https://www.codewars.com/kata/59e77930233243a7b7000026/train/javascript
 //console.log(amaroPlan(57), "kejk");
@@ -9,4 +10,7 @@ import { fortune } from "./Katas/BankersPlan";
 //console.log(whoTookTheCarKey(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110', '01100100', '01100101', '01110010']));
 
 // Bankers plan
-console.log(fortune(100000, 1, 2000, 15, 1));
+//console.log(fortune(100000, 1, 2000, 15, 1));
+
+// Beggars
+console.log(beggars([1,2,3,4,5, 4, 6, 8, 1, 6],3));
